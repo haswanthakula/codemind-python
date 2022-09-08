340eb18a1ac58895eeffa172e06f78d2232c6a06
Haswanth Akula <107489869+haswanthakula@users.noreply.github.com>
2022-09-08 20:06:18 +0530

    def isperfect(n):     c = 0     for i in range(1,n):         if n%i==0:             c+=i     if c==n:         return True     else:         return False
    n = int(input())
    res = isperfect(n)
    print(res)

--- Perfect_Number.py
+++ Perfect_Number.py
@@ -1,6 +1,13 @@
-n=int(input())
-c =0
-for i in range(1,n):
-    if n%i==0:
-        c+=i
-print(c==n)
+def isperfect(n):
+    c = 0
+    for i in range(1,n):
+        if n%i==0:
+            c+=i
+    if c==n:
+        return True
+    else:
+        return False
+
+n = int(input())
+res = isperfect(n)
+print(res)
