cdc67c7ee2d5790ae76f8ed830f35eaf623782a6
Haswanth Akula <107489869+haswanthakula@users.noreply.github.com>
2022-10-29 09:56:16 +0530

    def fun(n):     c = 0     for i in range(1,n):         if n % i == 0:             c += i     return c
    a = int(input())
    b = int(input())
    if (fun(b) == a and fun(a) == b):
        print('Amicable')
    else:
        print('Not Amicable')

---
+++ Amicable_Numbers.py
@@ -0,0 +1,13 @@
+def fun(n):
+    c = 0
+    for i in range(1,n):
+        if n % i == 0:
+            c += i
+    return c
+
+a = int(input())
+b = int(input())
+if (fun(b) == a and fun(a) == b):
+    print('Amicable')
+else:
+    print('Not Amicable')
